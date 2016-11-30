85409fa4a4f8509fef7f51d6dbb408dc2d3f4094
3people <smsn7031@naver.com>
2016-11-30 23:48:52 +0900

    merge

--- code/dropship_gomoku.py
+++ code/dropship_gomoku.py
@@ -81,18 +81,56 @@ class Board:
             self.printboard()
 >>>>>>> Stashed changes
 
+class Board:
+    def __init__(self):
+        self.board = [['+' for _ in range(20)] for _ in range(20)]
+        self.startgame()
+
+
+    def printboard(self) :
+        print("    1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20")
+        for x in range(20):
+            print(str(x+1).rjust(2), end = " ")
+            for y in range(20):
+                print(str(self.board[x][y]).rjust(2), end=" " if y < 19 else "\n")
+
+    def startgame(self):
+        self.printboard()
+        while True:
+            x = int(input("player1의 x좌표를 입력하시오 : "))
+            y = int(input("player1의 y좌표를 입력하시오 : "))
+            self.board[x-1][y-1] = "●"
+            self.printboard()
+            if self.check(x-1,y-1,"●"):
+                print("player1 win")
+                break;
+            x = int(input("player2의 x좌표를 입력하시오 : "))
+            y = int(input("player2의 y좌표를 입력하시오 : "))
+            self.board[x-1][y-1] = "○"
+            if self.check(x-1,y-1,"○"):
+                print("player2 win")
+                break;
+            self.printboard()
+
     def check(self, x, y, mark):
         a = []
         for n in range(1,5):
             a.append(eval("self.check"+str(n)+"("+str(x)+","+str(y)+",\""+mark+"\")"))
 <<<<<<< Updated upstream
         if 5 in a or (mark == "○" and True in [x >= 6 for x in a]):
+<<<<<<< HEAD
 =======
         if 5 in a:
 >>>>>>> Stashed changes
             return True
         else :
             return False
+=======
+            if 5 in a:
+                return True
+            else :
+                return False
+>>>>>>> master
 
     def check1(self, x, y, mark, d = 0):
         """가로방향"""
@@ -166,7 +204,10 @@ class Board:
                     return 0
         else :
             return 0
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> master
         if d == 0:
             return 1 + self.check1(x-1, y, mark, 1) + self.check1(x+1, y, mark, -1)
         elif d == 1:
@@ -224,6 +265,9 @@ class Board:
                 return 1 + self.check4(x+1, y-1, mark, -1)
             else :
                 return 0
+<<<<<<< HEAD
 >>>>>>> Stashed changes
+=======
+>>>>>>> master
 
 b = Board()
